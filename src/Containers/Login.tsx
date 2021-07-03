10e065dfafb683b9317b6d18dca515bf8d44569b
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-07-03 15:07:38 +0800

    react with ts init

---
+++ src/Containers/Login.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Login = ()=>{
+    return(
+        <>
+        <table>
+            <td>User</td>
+            <td>Guest</td>
+        </table>
+        </>
+    )
+}
+export default Login;
